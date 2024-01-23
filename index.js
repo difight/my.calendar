d2c99bf17f7fa41f09bbe8ec997b46cd4d745ca4
Andrey Bashkirev <greed_nord@mail.ru>
2024-01-23 17:21:56 +0300

    init project

---
+++ index.js
@@ -0,0 +1,9 @@
+import 'dotenv/config'
+import pkg from 'telegraf';
+const { Telegraf } = pkg;
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
