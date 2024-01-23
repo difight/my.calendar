7ee1a2c484b2cae75e2d1aa5c223c18b0aa16863
Andrey Bashkirev <greed_nord@mail.ru>
2024-01-23 23:57:28 +0300

    add middleware and catch error

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ import 'dotenv/config'
 import pkg from 'telegraf';
 const { Telegraf } = pkg;
 const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.use(async (ctx, next) => {
+    const start = new Date()
+    await next()
+    const ms = new Date() - start
+    console.log('Response time: %sms', ms)
+})
+bot.catch((err, ctx) => {
+    console.log(`Ooops, encountered an error for ${ctx.updateType}`, err)
+})
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
